abb603d42a2554ae687d395fe5c0b8120d21e013
ravikesh <ravikesh2000indurkar@gmail.com>
2024-07-08 21:54:26 +0530

    added insert

--- script.js
+++ script.js
@@ -4,12 +4,29 @@ async function fun(){
     let sh = document.getElementById('showdata')
     let d = c.map((e)=>`
     <tr>
+        <td>${e.id}</td>
         <td>${e.name}</td>
         <td>${e.age}</td>
         <td>${e.marks}</td>
-        </tr>
+    </tr>
 
     `).join("")
     sh.innerHTML = d
 }
 fun()
+
+function insert1(){
+    let data ={
+        id:document.getElementById('id').value,
+        name:document.getElementById('name').value,
+        age:document.getElementById('age').value,
+        marks:document.getElementById('marks').value
+    }
+    fetch("http://localhost:3000/student" , {
+        method:"POST",
+        headers:{   
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(data),
+    })
+}
