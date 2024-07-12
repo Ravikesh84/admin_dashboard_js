3c56bc9646087201a68f36ae5dadf71e2fe14e99
ravikesh <ravikesh2000indurkar@gmail.com>
2024-07-12 23:05:46 +0530

    added api insert javascript

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ async function fun(){
     <tr>
         <td>${e.id}</td>
         <td>${e.name}</td>
-        <td>${e.age}</td>
-        <td>${e.marks}</td>
+        <td>${e.author}</td>
+       
         <td> <button onclick="mydelete(${e.id})"> Delete </button> </td>
+        <td> <button onclick="myupdate(${e.id})"> Edit </button> </td>
     </tr>
 
     `).join("")
@@ -28,8 +29,7 @@ function insert1(){
     let data ={
         id:document.getElementById('id').value,
         name:document.getElementById('name').value,
-        age:document.getElementById('age').value,
-        marks:document.getElementById('marks').value
+        author:document.getElementById('author').value,
     }
     fetch("http://localhost:3000/student" , {
         method:"POST",
@@ -38,4 +38,42 @@ function insert1(){
         },
         body:JSON.stringify(data),
     })
+}
+
+// var storeid = 0
+// function myupdate(id){
+//     storeid = id;
+//     document.getElementById("edit").style.display="block"
+//     document.getElementById('id1').value=id
+// }
+
+// function update(){
+//     let mydata = {
+//         id:document.getElementById('id1').value,
+//         name:document.getElementById('name1').value,
+//         age:document.getElementById('age1').value,
+//         marks:document.getElementById('marks1').value
+
+//     }
+//     fetch(`http://localhost:3000/student/${storeid}`,{
+//         method:"PUT",
+//         headers:{
+//             "Content-Type":"application/json"
+//             },
+//             body:JSON.stringify(mydata)
+//     })
+//     .then((res)=>alert("Update Successfully"))
+//     .catch(res=>alert("error"))
+// }
+
+async function myupdate(){
+    let mydata = await fetch("http://localhost:3000/student")
+    let con = await mydata.json()
+    let show = document.getElementById('demo')
+    let y = con.map((e)=>`
+    <input type="text" value="${e.id}"> <br>
+    <input type="text" value="${e.name}"> <br>
+    <input type="text" value="${e.author}"> <br>
+    `)
+    show.innerHTML=y
 }
